774e10ba2913b90942461b46db87bdcd557b678c
Prathap Ramachandra <prathap.ramachandra.ruby@gmail.com>
2011-07-26 23:13:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoothRunner::Application.config.secret_token = '9a62768697b159826faa6455a10e078db37642af23734d5c788c5f8a47b778722acce4b148035bcd4572e24ec2a4c0a7e2d4ac04087f6332ffe54cd6bcaef1fc'
